f331d1412d36f1df4a540487fad0f314324f8abf
Bernardo <cebernardosilva50@gmail.com>
2023-12-21 07:46:36 -0300

    # segundo checkpoint.  primeiro modulo

--- .history/core/settings_20231221065921.py
+++ .history/core/settings_20231221065921.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'postgres',
-        'PASSWORD': '051974',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'OPTIONS': {
